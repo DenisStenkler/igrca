1610b3d68adab9f63f6da5528589932db0c266cb
DESKTOP-ONTMBAI\Denis Štenkler <denis.stenkler@gmail.com>
2018-03-21 11:42:23 +0100

    OSNOVNA HTML STRAN IN NARJEN CANVAS ELEMENT

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // JavaScript source code
+
+var canvas = document.getElementById('MojCanvas');
+
+canvas.width = window.innerWidth - 25;
+canvas.height = window.innerHeight -25;
+
+var kaca = canvas.getContext("2d"); 
+
+kaca.fillRect(50, 50, 15, 15);
+
+
+
